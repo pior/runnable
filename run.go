ee4e4f2a793d35d59e4a6dbe3756ae1fe1dd8c90
Pior Bastida <pior@pbastida.net>
2020-03-08 16:28:07 -0400

    Add a method to execute a single runnable with signal handling

--- run.go
+++ run.go
@@ -13,3 +13,12 @@ func RunGroup(runners ...Runnable) {
 		stdlog.Fatal(err)
 	}
 }
+
+// Run runs a single runnable, and listen to SIGTERM/SIGINT
+func Run(runner Runnable) {
+	ctx := context.Background()
+	err := Signal(runner).Run(ctx)
+	if err != nil && err != context.Canceled {
+		stdlog.Fatal(err)
+	}
+}
