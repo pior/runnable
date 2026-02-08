e32b5a7294906eb518f30338210bcb129bfec4c6
Pior Bastida <pior@pbastida.net>
2026-02-08 12:14:18 +0100

    Fix canceled context issue in closer

--- closer.go
+++ closer.go
@@ -4,22 +4,26 @@ import (
 	"context"
 )
 
-// Closer returns a runnable intended to call a Close method on shutdown.
+// Closer returns a runnable that calls Close on context cancellation.
+// There is no timeout on the call to Close.
 func Closer(c interface{ Close() }) Runnable {
-	return &closer{"closer/" + runnableName(c), func(ctx context.Context) error {
+	return &closer{"closer/" + runnableName(c), func(_ context.Context) error {
 		c.Close()
 		return nil
 	}}
 }
 
-// CloserErr returns a runnable intended to call a Close method on shutdown.
+// CloserErr returns a runnable that calls Close on context cancellation.
+// There is no timeout on the call to Close.
 func CloserErr(c interface{ Close() error }) Runnable {
-	return &closer{"closer/" + runnableName(c), func(ctx context.Context) error {
+	return &closer{"closer/" + runnableName(c), func(_ context.Context) error {
 		return c.Close()
 	}}
 }
 
-// CloserCtx returns a runnable intended to call a Close method on shutdown.
+// CloserCtx returns a runnable that calls Close on context cancellation.
+// The context passed to Close is not cancelled, so Close can perform graceful cleanup.
+// There is no timeout on the call to Close.
 func CloserCtx(c interface{ Close(context.Context) }) Runnable {
 	return &closer{"closer/" + runnableName(c), func(ctx context.Context) error {
 		c.Close(ctx)
@@ -27,7 +31,9 @@ func CloserCtx(c interface{ Close(context.Context) }) Runnable {
 	}}
 }
 
-// CloserCtxErr returns a runnable intended to call a Close method on shutdown.
+// CloserCtxErr returns a runnable that calls Close on context cancellation.
+// The context passed to Close is not cancelled, so Close can perform graceful cleanup.
+// There is no timeout on the call to Close.
 func CloserCtxErr(c interface{ Close(context.Context) error }) Runnable {
 	return &closer{"closer/" + runnableName(c), func(ctx context.Context) error {
 		return c.Close(ctx)
@@ -43,7 +49,7 @@ func (c *closer) runnableName() string { return c.name }
 
 func (c *closer) Run(ctx context.Context) error {
 	<-ctx.Done()
-	err := c.closeFn(ctx)
+	err := c.closeFn(context.WithoutCancel(ctx))
 	if err != nil {
 		return &RunnableError{"closer: Close() returned an error", err}
 	}
